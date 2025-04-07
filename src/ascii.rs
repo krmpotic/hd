4e2f76d3d9f7d48a8a1007a4ce140b0eac99dbde
Luka Krmpotić <luka.krmpotic@gmail.com>
2025-04-08 01:07:59 +0200

    add values to Ascii enum

--- src/ascii.rs
+++ src/ascii.rs
@@ -1,43 +1,53 @@
 #[derive(Debug, PartialEq)]
-enum Ascii {
-    LowChar,
-    BigChar,
-    Symbol,
-    Digit,
-    Whitespace,
+enum Special {
+    Null,
+    Delete,
+    Other,
 }
 
-pub fn to_canonical(c: u8) -> char {
-    match is_ascii(c) {
-        Some(Ascii::LowChar) => c as char,
-        Some(Ascii::BigChar) => c as char,
-        Some(Ascii::Digit) => c as char,
-        Some(Ascii::Whitespace) => ' ',
-        Some(Ascii::Symbol) => c as char,
+#[derive(Debug, PartialEq)]
+enum Ascii {
+    Special(Special),
+    LowChar(char),
+    BigChar(char),
+    Symbol(char),
+    Digit(u8),
+    Whitespace(char),
+}
+
+pub fn to_canonical(b: u8) -> char {
+    match is_ascii(b) {
+        Some(Ascii::LowChar(c)) => c,
+        Some(Ascii::BigChar(c)) => c,
+        Some(Ascii::Digit(_)) => b as char,
+        Some(Ascii::Whitespace(_)) => ' ',
+        Some(Ascii::Symbol(c)) => c,
+        Some(Ascii::Special(_)) => '#',
         None => '?',
     }
 }
 
 fn is_ascii(c: u8) -> Option<Ascii> {
     if b'a' <= c && c <= b'z' {
-        Some(Ascii::LowChar)
+        Some(Ascii::LowChar(c as char))
     } else if b'A' <= c && c <= b'Z' {
-        Some(Ascii::BigChar)
+        Some(Ascii::BigChar(c as char))
     } else if b'0' <= c && c <= b'9' {
-        Some(Ascii::Digit)
-    } else if c == b'\n' || c == b' ' || c == b'\t' {
-        Some(Ascii::Whitespace)
-    } else if c == b'['
-        || c == b']'
-        || c == b'('
-        || c == b')'
-        || c == b'{'
-        || c == b'}'
-        || c == b'='
-        || c == b'"'
-        || c == b'\''
+        Some(Ascii::Digit(c - b'0'))
+    } else if c == b'\n' || c == b' ' || c == b'\t' || c == b'\t' || c == 0x0b {
+        Some(Ascii::Whitespace(c as char))
+    } else if (c >= 0x21 && c <= 0x2f)
+        || (c >= 0x3a && c <= 0x40)
+        || (c >= 0x5b && c <= 0x60)
+        || (c >= 0x7b && c <= 0x7e)
     {
-        Some(Ascii::Symbol)
+        Some(Ascii::Symbol(c as char))
+    } else if c <= 0x20 || c == 0x7f {
+        match c {
+            0x00 => Some(Ascii::Special(Special::Null)),
+            0x7f => Some(Ascii::Special(Special::Delete)),
+            _ => Some(Ascii::Special(Special::Other)),
+        }
     } else {
         None
     }
@@ -46,7 +56,10 @@ fn is_ascii(c: u8) -> Option<Ascii> {
 #[test]
 fn test_is_ascii() {
     assert_eq!(is_ascii(0xff), None);
-    assert_eq!(is_ascii(b'c'), Some(Ascii::LowChar));
-    assert_eq!(is_ascii(b'C'), Some(Ascii::BigChar));
-    assert_eq!(is_ascii(b'\n'), Some(Ascii::Whitespace));
+    assert_eq!(is_ascii(b'c'), Some(Ascii::LowChar('c')));
+    assert_eq!(is_ascii(b'C'), Some(Ascii::BigChar('C')));
+    assert_eq!(is_ascii(b'\n'), Some(Ascii::Whitespace('\n')));
+    assert_eq!(is_ascii(b'3'), Some(Ascii::Digit(3)));
+    assert_eq!(is_ascii(0x7f), Some(Ascii::Special(Special::Delete)));
+    assert_eq!(is_ascii(0x00), Some(Ascii::Special(Special::Null)));
 }
